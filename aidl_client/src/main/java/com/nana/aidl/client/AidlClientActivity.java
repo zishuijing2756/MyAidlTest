bc4367fe757e1660142518de3b7ee53f4325be63
yangnn <yangnn1@guahao.com>
2018-10-11 13:58:39 +0800

    Initial commit

---
+++ aidl_client/src/main/java/com/nana/aidl/client/AidlClientActivity.java
@@ -0,0 +1,13 @@
+package com.nana.aidl.client;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AidlClientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aidl_client);
+    }
+}
