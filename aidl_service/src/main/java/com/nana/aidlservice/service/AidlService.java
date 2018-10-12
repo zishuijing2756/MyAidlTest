a08a03c9b82beca1a7e039b9fed954731aae66aa
yangnn <yangnn1@guahao.com>
2018-10-12 10:36:34 +0800

    初始化服务接口

--- aidl_service/src/main/java/com/nana/aidlservice/service/AidlService.java
+++ aidl_service/src/main/java/com/nana/aidlservice/service/AidlService.java
@@ -3,7 +3,11 @@ package com.nana.aidlservice.service;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.RemoteException;
 import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.nana.aidlservice.IServerAidlInterface;
 
 /**
  * Description: 服务端提供给客户端的服务
@@ -13,13 +17,38 @@ import android.support.annotation.Nullable;
  * @since 1.3.2
  */
 public class AidlService extends Service {
+    private static final String TAG="AidlService";
 
-    private ServerAidlInterfaceImpl process;
+    private IServerAidlInterface.Stub stub=new IServerAidlInterface.Stub() {
+        @Override
+        public void basicTypes(int anInt, long aLong, boolean aBoolean, float aFloat, double aDouble, String aString) throws RemoteException {
+
+        }
+
+        @Override
+        public int getInitNum() throws RemoteException {
+            return 0;
+        }
+    };
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
+        if(stub!=null){
+           return stub;
+        }
         return null;
+    }
+
+    public int getInitNum(){
+        if(stub!=null){
+            try {
+                return stub.getInitNum();
+            } catch (RemoteException e) {
+                Log.i(TAG,e.getMessage());
+            }
+        }
+        return 0;
     }
 
     @Override
